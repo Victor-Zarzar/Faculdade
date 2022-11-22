9dc77e1c8d73e7de903cff895352100f7d28e401
Victor Zarzar <114430780+Victor-Zarzar@users.noreply.github.com>
2022-11-22 22:01:27 +0000

    Faculdade

---
+++ Linguagem C/faculdade4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float altura[3];
+    altura[0] = 1.50;
+    altura[1] = 1.80;
+    altura[2] = 2.02;
+    printf("\n Vetor Altura[0] = %f", altura[0]);
+    printf("\n Vetor Altura[1] = %f", altura[1]);
+    printf("\n Vetor Altura[2] = %f", altura[2]);
+    return 0;
+}
